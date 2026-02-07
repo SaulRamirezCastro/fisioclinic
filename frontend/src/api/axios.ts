e38314182f7ad1e89bdc71f24bb967e45c15266f
Saúl Ramriez <chuy1106@gmail.com>
2026-02-06 22:44:48 -0600

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = process.env.DJANGO_API;
+
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: API_BASE_URL,
 });
 
 // 🔹 Agregar access token
