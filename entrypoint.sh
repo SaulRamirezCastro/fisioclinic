6ce610533760351508b39cd5b18bcfb4f4856448
Saúl Ramriez <chuy1106@gmail.com>
2026-02-13 08:47:05 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-set -e
 
-echo "Checking Python dependencies..."
-pip install --no-cache-dir -r requirements.txt
+echo "Waiting for PostgreSQL..."
+
+while ! nc -z $DB_HOST 5432; do
+  sleep 1
+done
+
+echo "Database ready"
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
 exec "$@"
