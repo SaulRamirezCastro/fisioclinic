4fb46723cc9e494bc924b32d021dc6ecaf176d69
Saúl Ramriez <chuy1106@gmail.com>
2026-02-14 20:36:21 -0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -82,11 +82,14 @@ SESSION_COOKIE_HTTPONLY = True
 # ===================================
 # Render provee DATABASE_URL automáticamente
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+ 'default': {
+  'ENGINE': 'django.db.backends.postgresql',
+  'NAME': os.getenv('DB_NAME'),
+  'USER': os.getenv('DB_USER'),
+  'PASSWORD': os.getenv('DB_PASSWORD'),
+  'HOST': os.getenv('DB_HOST'),
+  'PORT': os.getenv('DB_PORT'),
+ }
 }
 
 # ===================================
